6705aa09c1ad0b2eb75d672c78502f92cd80b311
David Belcher <davidjamesbelcher0@gmail.com>
2019-08-08 07:02:26 +1000

    inital commit, npm start to run

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const imagePaths = {
+    minus: "https://images.vexels.com/media/users/3/131484/isolated/preview/a432fa4062ed3d68771db7c1d65ee885-minus-inside-circle-icon-by-vexels.png",
+    plus: "https://cdn3.iconfinder.com/data/icons/glypho-generic-icons/64/plus-big-512.png"
 }
 
-export default App;
+class App extends React.Component{
+    state = {
+        open: true
+    }
+    toggleImage = () => {
+        this.setState(state => ({open: !state.open}))
+    }
+
+    getImageName = () => this.state.open ? 'plus' : 'minus'
+
+    render() {
+        const imageName = this.getImageName();
+        return(
+            <div>
+                <div className="App">
+                    <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Don't tell me what to do!
+                        <div>
+                          <img style={{maxWidth:'50px'}} src={imagePaths[imageName]} onClick={this.toggleImage} />
+                        </div>
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                    </header>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App; 
